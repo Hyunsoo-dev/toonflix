7c0838375f01dd16e09e6c5faca143bccd1612ce
Hyunsoo-dev <jing07161@gmail.com>
2024-03-01 16:50:28 +0900

    Feature: 뽀모도로 구현

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
@@ -9,6 +12,54 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  static const twentyFiveMinutes = 1500;
+  int totalSeconds = twentyFiveMinutes;
+  int totalPomodoros = 0;
+  bool isPlay = false;
+
+  late Timer timer;
+
+  void onTick(Timer timer) {
+    if (totalSeconds == 0) {
+      setState(() {
+        totalPomodoros = totalPomodoros + 1;
+        isPlay = false;
+        totalSeconds = twentyFiveMinutes;
+      });
+      timer.cancel();
+    } else {
+      setState(() {
+        totalSeconds = totalSeconds - 1;
+      });
+    }
+  }
+
+  void onStartPressed() {
+    timer = Timer.periodic(const Duration(seconds: 1), onTick);
+    setState(() {
+      isPlay = true;
+    });
+  }
+
+  void onPausePressed() {
+    timer.cancel();
+    setState(() {
+      isPlay = false;
+    });
+  }
+
+  void onPressReset() {
+    timer.cancel();
+    setState(() {
+      totalSeconds = twentyFiveMinutes;
+      isPlay = false;
+    });
+  }
+
+  String format(int seconds) {
+    return Duration(seconds: seconds).toString().split(".")[0].substring(2, 7);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,22 +71,45 @@ class _HomeScreenState extends State<HomeScreen> {
               child: Container(
                 alignment: Alignment.center,
                 child: Text(
-                  "25:00",
+                  format(totalSeconds),
                   style: TextStyle(
-                      color: Theme.of(context).cardColor,
-                      fontSize: 89,
-                      fontWeight: FontWeight.w600),
+                    color: Theme.of(context).cardColor,
+                    fontSize: 89,
+                    fontWeight: FontWeight.w600,
+                  ),
                 ),
               ),
             ),
             Flexible(
               flex: 2,
               child: Center(
-                child: IconButton(
-                  icon: const Icon(Icons.play_circle_outline_rounded),
-                  onPressed: () {},
-                  iconSize: 120,
-                  color: Theme.of(context).cardColor,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    isPlay
+                        ? IconButton(
+                            icon: const Icon(
+                              Icons.stop_circle_outlined,
+                            ),
+                            onPressed: onPausePressed,
+                            iconSize: 120,
+                            color: Theme.of(context).cardColor,
+                          )
+                        : IconButton(
+                            icon: const Icon(
+                              Icons.play_circle_outline_rounded,
+                            ),
+                            onPressed: onStartPressed,
+                            iconSize: 120,
+                            color: Theme.of(context).cardColor,
+                          ),
+                    IconButton(
+                      onPressed: onPressReset,
+                      icon: const Icon(Icons.refresh_rounded),
+                      color: Theme.of(context).cardColor,
+                      iconSize: 50,
+                    )
+                  ],
                 ),
               ),
             ),
@@ -45,20 +119,24 @@ class _HomeScreenState extends State<HomeScreen> {
                 children: [
                   Expanded(
                     child: Container(
-                      decoration:
-                          BoxDecoration(color: Theme.of(context).cardColor),
-                      child: const Column(
+                      decoration: BoxDecoration(
+                        color: Theme.of(context).cardColor,
+                        borderRadius: const BorderRadius.only(
+                            topLeft: Radius.circular(50),
+                            topRight: Radius.circular(50)),
+                      ),
+                      child: Column(
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: [
-                          Text(
+                          const Text(
                             "Pomodoros",
                             style: TextStyle(
                               fontSize: 20,
                               fontWeight: FontWeight.w600,
                             ),
                           ),
-                          Text("0",
-                              style: TextStyle(
+                          Text("$totalPomodoros",
+                              style: const TextStyle(
                                 fontSize: 58,
                                 fontWeight: FontWeight.w600,
                               )),
